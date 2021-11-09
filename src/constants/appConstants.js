b6e0492d835bfe8c3eb09bd83d05f8c7607ef971
hhoury <hhoury@hotmail.com>
2021-11-09 23:54:51 +0200

    server ip

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,2 +1,2 @@
-// export const API = 'http://109.235.69.20/ahmad/api/'
-export const API = 'https://localhost:44362/api/'
+export const API = 'http://109.235.69.20/ahmad/api/'
+// export const API = 'https://localhost:44362/api/'
