a3dc43aa3ac397609928e5f7cdf5e23776be20fa
hhoury <hhoury@hotmail.com>
2021-11-20 15:02:11 +0200

    nm

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,2 +1,3 @@
-// export const API = 'http://109.235.69.20/ahmad/api/'
-export const API = 'https://localhost:44362/api/'
+export const API = 'http://109.235.69.20/ahmad/api/'
+// export const API = 'https://localhost:44362/api/'
+// export const API = 'https://192.168.0.104:44362/api/'
